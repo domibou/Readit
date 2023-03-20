f8afafe95a507508877acad76166d914baa694c5
Dominic Boucher <dominic.boucher.3ulaval.ca>
2023-03-19 20:18:35 -0400

    commit

---
+++ app/static/js/main.js
@@ -0,0 +1,15 @@
+const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("click", login);
+
+const signUpButton = document.getElementById("signUpButton");
+signUpButton.addEventListener("click", signUp());
+
+function login()
+{
+    console.log("log in these peanuts");
+}
+
+function signUp()
+{
+    console.log("sign these peanuts");
+}
